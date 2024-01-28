77919f2c02b801e1d6275724fcf4b83df7775293
Aleix <feina.aleix@gmail.com>
2024-01-28 16:07:06 +0100

    try dataframe show

--- src/main/scala/com/aleix/dataproc/Demo1.scala
+++ src/main/scala/com/aleix/dataproc/Demo1.scala
@@ -12,14 +12,16 @@ object Demo1 {
     val spark = SparkSession.builder.getOrCreate()
     spark.sparkContext.setLogLevel("ERROR") // only affects spark framework messages
 
-//    val data = Seq(
-//      ("A", "Categoría1", 10, "..."),
-//      ("A", "Categoría2", 20, "---"),
-//      ("B", "Categoría1", 30, "´´´"),
-//      ("B", "Categoría2", 40, "^^^")
-//    )
-//
-//    spark.createDataFrame(data).toDF("id", "cat", "valor", "not_important").show()
+    import spark.implicits._
+
+    val data = Seq(
+      ("A", "Categoría1", 10, "..."),
+      ("A", "Categoría2", 20, "---"),
+      ("B", "Categoría1", 30, "´´´"),
+      ("B", "Categoría2", 40, "^^^")
+    )
+
+    spark.createDataFrame(data).toDF("id", "cat", "valor", "not_important").show()
 
     logger.info("Hello world!")
 
