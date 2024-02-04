8a561d288069e05750bd0958bb1afbe5330aad2d
Aleix <feina.aleix@gmail.com>
2024-02-04 17:11:47 +0100

    --enable-component-gateway + uniform_bucket_level_access = true

--- terraform/apis.tf
+++ terraform/apis.tf
@@ -8,3 +8,9 @@ resource "google_project_service" "cloudresource_api" {
   service = "cloudresourcemanager.googleapis.com"
   project = var.project_id
 }
+
+# in order to view the monitoring API scope in OAuth 2.0 Playground
+resource "google_project_service" "monitoring_api" {
+  service = "monitoring.googleapis.com"
+  project = var.project_id
+}
--- terraform/buckets.tf
+++ terraform/buckets.tf
@@ -1,14 +1,17 @@
 resource "google_storage_bucket" "stg_bucket" {
   name = "aleix-stg-bucket"
   location = var.multiregion
+  uniform_bucket_level_access = true
 }
 
 resource "google_storage_bucket" "tmp_bucket" {
   name = "aleix-tmp-bucket"
   location = var.multiregion
+  uniform_bucket_level_access = true
 }
 
 resource "google_storage_bucket" "demos_bucket" {
   name = "aleix-demos-bucket"
   location = var.multiregion
+  uniform_bucket_level_access = true
 }
