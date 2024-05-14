54bf19e55ad1d38ade7f7b1d2a4967c06ffd6a43
Aleix <feina.aleix@gmail.com>
2024-05-14 10:39:13 +0200

    use correct timezone

--- src/main/scala/com/spark/SparkSessionUtils.scala
+++ src/main/scala/com/spark/SparkSessionUtils.scala
@@ -7,15 +7,19 @@ import org.apache.spark.sql.SparkSession
 object SparkSessionUtils {
 
   def getSparkSession(appName: String, executionMode: ExecutionMode = ExecutionMode.GCP): SparkSession = {
+    val timezone = "Europe/Sofia"
+
     if (executionMode == ExecutionMode.local) {
       SparkSession.builder
         .master("local[*]")
         .appName(appName)
+        .config("spark.sql.session.timeZone", timezone)
         .getOrCreate()
     }
     else {
       SparkSession.builder
         .appName(appName)
+        .config("spark.sql.session.timeZone", timezone)
         .getOrCreate()
     }
 
