d240c7a4e8af3d60ea7a5eb2c0101c6da6458e25
Aleix <feina.aleix@gmail.com>
2024-05-26 19:23:21 +0200

    buket region change from multiregional to regional

--- terraform/buckets.tf
+++ terraform/buckets.tf
@@ -1,15 +1,27 @@
 
+// REQUIRED
+
 resource "google_storage_bucket" "dataproc_stg_bucket" {
   name = "dataproc-stg-bucket"
-  location = var.multiregion
+  location = var.region
 
   uniform_bucket_level_access = true
   force_destroy = true
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+
+    condition {
+      age = 1 # days
+    }
+  }
 }
 
 resource "google_storage_bucket" "dataproc_tmp_bucket" {
   name = "dataproc-tmp-bucket"
-  location = var.multiregion
+  location = var.region
 
   uniform_bucket_level_access = true
   force_destroy = true
@@ -20,58 +32,14 @@ resource "google_storage_bucket" "dataproc_tmp_bucket" {
     }
 
     condition {
-      age = 14 # days
+      age = 1 # days
     }
   }
-}
-
-resource "google_storage_bucket" "bq_tmp_bucket" {
-  name = "bq-tmp-bucket"
-  location = var.multiregion
-
-  uniform_bucket_level_access = true
-  force_destroy = true
-
-  lifecycle_rule {
-    action {
-      type = "Delete"
-    }
-
-    condition {
-      age = 14 # days
-    }
-  }
-}
-
-resource "google_storage_bucket" "aleix_demos_bucket" {
-  name = "aleix-demos-bucket"
-  location = var.multiregion
-
-  uniform_bucket_level_access = true
-  force_destroy = true
 }
 
 resource "google_storage_bucket" "spark_history_bucket" {
   name = "spark-history-bucket"
-  location = var.multiregion
-
-  uniform_bucket_level_access = true
-  force_destroy = true
-
-  lifecycle_rule {
-    action {
-      type = "Delete"
-    }
-
-    condition {
-      age = 14 # days
-    }
-  }
-}
-
-resource "google_storage_bucket" "aleix_tmp_bucket" {
-  name = "aleix-tmp-bucket"
-  location = var.multiregion
+  location = var.region
 
   uniform_bucket_level_access = true
   force_destroy = true
@@ -83,6 +51,24 @@ resource "google_storage_bucket" "aleix_tmp_bucket" {
 
     condition {
       age = 7 # days
+    }
+  }
+}
+
+resource "google_storage_bucket" "bq_tmp_bucket" {
+  name = "bq-tmp-bucket"
+  location = var.region
+
+  uniform_bucket_level_access = true
+  force_destroy = true
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+
+    condition {
+      age = 1 # days
     }
   }
 }
@@ -101,6 +87,34 @@ resource "google_storage_bucket" "project_id_cloudbuild"{
 
     condition {
       age = 1 # days
+    }
+  }
+}
+
+// OPTIONAL
+
+resource "google_storage_bucket" "aleix_demos_bucket" {
+  name = "aleix-demos-bucket"
+  location = var.region
+
+  uniform_bucket_level_access = true
+  force_destroy = true
+}
+
+resource "google_storage_bucket" "aleix_tmp_bucket" {
+  name = "aleix-tmp-bucket"
+  location = var.region
+
+  uniform_bucket_level_access = true
+  force_destroy = true
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+
+    condition {
+      age = 7 # days
     }
   }
 }
